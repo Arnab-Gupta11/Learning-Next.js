ff2dbfb25609be1d679482d633ec3d7b4ada178e
Arnab Gupta <arnabgupta1111@gmail.com>
2024-05-08 19:10:42 +0600

    Client side data fetching

---
+++ 2.data-fetching/src/components/Users/Users.js
@@ -0,0 +1,31 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const fetchUser = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    setUsers(data);
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <div className="max-w-lg mx-auto">
+      <h2 className="text-3xl font-bold my-10 text-center">Total Users: {users.length}</h2>
+      {users.map((user) => {
+        return (
+          <div key={user.id} className="card w-full bg-emerald-200 shadow-xl mx-auto mt-5">
+            <div className="card-body">
+              <h2 className="card-title">{user.name}</h2>
+              <p>Email: {user.email}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Users;
